4db51cf30ebb21fddfe05e56c20166e5fda586c5
Tuan Nguyen <trentontuannguyen@gmail.com>
2023-06-29 16:23:44 -0400

    wip

--- lib/looping.py
+++ lib/looping.py
@@ -2,7 +2,15 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i = 10
+    # since this is a reverse loop
+    # the range is 10 to 0 but not incl. 0
+    # the 3rd parameter states specifies the decrementing value with -1
+    while i in range(10, 0, -1):
+        print(i)
+        i -= 1 
+    print("Happy New Year!")
+
 
 def square_integers(int_list):
     # code goes here!
