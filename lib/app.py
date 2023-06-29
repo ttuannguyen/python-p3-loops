22c6cb94352a1dc8aed5d8825c99b23c74552653
Tuan Nguyen <trentontuannguyen@gmail.com>
2023-06-29 00:36:15 -0400

    wip

---
+++ lib/app.py
@@ -0,0 +1,27 @@
+# WHILE LOOP
+# i = 0
+# while i < 5:
+#     print("Looping!")
+#     i += 1
+
+# FOR LOOP
+# for i in range (8):
+#     print(f"Looping! i is: {i}") 
+
+
+# LIST COMPREHENSION
+player_heights = [0.008, 0.008, 0.008, 0.009, 0.008, 0.01, 0.009, 0.009, 0.01, 0.008, 0.009, 0.009, 0.008, 0.008, 0.008, 0.009, 0.008, 0.009, 0.01, 0.01]
+
+# regular method:
+# inch_heights = list()
+# for height in player_heights:
+#     inch_height = height * 7920
+#     inch_heights.append(inch_height)
+
+# print(inch_heights)
+
+# list comprehension method:
+# player_heights = [height * 7920 for height in player_heights]
+# print(player_heights)
+
+
